65e7c8d83aa7a5a8277351206f174203c85510f7
Guz Alex <kalimatas@gmail.com>
2011-04-20 15:21:50 +0400

    Changed inde view

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -3,6 +3,7 @@
 class ErrorController extends Zend_Controller_Action {
 
     public function errorAction() {
+        $this->view->headTitle()->append('error');
         $errors = $this->_getParam('error_handler');
         
         switch ($errors->type) {
