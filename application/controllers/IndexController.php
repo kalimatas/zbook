8e01fbeba5517088cdaa9cbae90d448910571f9e
Guz Alex <kalimatas@gmail.com>
2011-04-22 16:51:24 +0400

    Working with Zend_Db_Table

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,8 @@ class IndexController extends Zend_Controller_Action {
 
     public function indexAction() {
         $entries = new Entry();
+        Zend_Debug::dump($entries->fetchAll()->count());
+        echo '----------------------------------------';
         $result = $entries->fetchLatest();
         Zend_Debug::dump($result);
     }
--- application/models/Entry.php
+++ application/models/Entry.php
@@ -9,9 +9,40 @@ class Entry extends Zend_Db_Table {
      * Fetch latest entries
      */
     public function fetchLatest($count = 10) {
+        $db = Zend_Registry::get('db');
+        //$select = new Zend_Db_Select($db);
+        $select = $db->select();
+        $select->from('entry')
+               ->where('id = ?', 2);
+        $result = $select->query();
+        //Zend_Debug::dump($result);
+        Zend_Debug::dump($result->fetchAll());
+        echo '----------------------------------------';
+
+        // insert an entry
+        $data = array('title' => 'From DB Insert',
+                      'announce' => 'This entry was created with DB Insert',
+                      'content' => 'Content of this entry',
+                      'date' => new Zend_Db_Expr('NOW()'),
+                      'author_id' => 1
+                     );
+        //$db->insert('entry', $data);
+        //echo 'LastInsertId: ' . $db->lastInsertId().'<br />';
+
+        // update an entry 3
+        $datau = array('title' => 'From DB Update1');
+        $where = $db->quoteInto('title = ?', 'From DB Update');
+        $db->update('entry', $datau, $where);
+        $first = $this->fetchRow('id=2');
+        $first->title = 'The first new row';
+        $first->save();
+
+        // user Zend_Db_Table_Abstract
+        //$this->insert($data);
+        //$this->delete($this->getAdapter()->quoteInto('id = ?', 1));
+
+
         $entries = $this->fetchAll(null, 'date DESC', $count);
-        //$entries = $this->find('id = 1');
-        //$entries = $this->fetchRow('id = 1');
         return $entries->toArray();
     }
 }
