140b22a27e581117bc6f8db5d79e946f86a2b033
Guz Alex <kalimatas@gmail.com>
2011-04-22 18:12:02 +0400

    Added BD dump

---
+++ data/zbook.sql
@@ -0,0 +1,82 @@
+-- MySQL dump 10.13  Distrib 5.5.9, for Linux (i686)
+--
+-- Host: localhost    Database: zbook
+-- ------------------------------------------------------
+-- Server version	5.5.9
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Author`
+--
+
+DROP TABLE IF EXISTS `Author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Author`
+--
+
+LOCK TABLES `Author` WRITE;
+/*!40000 ALTER TABLE `Author` DISABLE KEYS */;
+INSERT INTO `Author` VALUES (1,'kalimatas','Guz Alex','kalimatas@gmail.com','5f4dcc3b5aa765d61d8327deb882cf99'),(3,'kohutapu','Another user','a.guz13@yahoo.com','5f4dcc3b5aa765d61d8327deb882cf99');
+/*!40000 ALTER TABLE `Author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `entry`
+--
+
+DROP TABLE IF EXISTS `entry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entry` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `announce` text NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL,
+  `author_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entry`
+--
+
+LOCK TABLES `entry` WRITE;
+/*!40000 ALTER TABLE `entry` DISABLE KEYS */;
+INSERT INTO `entry` VALUES (10,'From DB Insert','This entry was created with DB Insert','Content of this entry','2011-04-22 17:13:25',1),(11,'From DB Insert','This entry was created with DB Insert','Content of this entry','2011-04-22 17:29:49',1),(2,'The first new row','Another announce for the second entry. Another announce for the second entry. Another announce for the second entry. ','Text for the second entry. Text for the second entry. Text for the second entry. Text for the second entry. Text for the second entry. ','2011-04-21 15:44:02',3),(3,'From DB Update1','This entry was created with DB Insert','Content of this entry','2011-04-22 16:05:49',3),(4,'From DB Update1','This entry was created with DB Insert','Content of this entry','2011-04-22 16:05:54',1),(5,'From DB Insert','This entry was created with DB Insert','Content of this entry','2011-04-22 16:26:01',1),(6,'From DB Insert','This entry was created with DB Insert','Content of this entry','2011-04-22 16:37:22',1),(7,'From DB Insert','This entry was created with DB Insert','Content of this entry','2011-04-22 16:38:19',3),(8,'From DB Insert','This entry was created with DB Insert','Content of this entry','2011-04-22 16:39:08',1),(9,'From DB Insert','This entry was created with DB Insert','Content of this entry','2011-04-22 16:41:13',1);
+/*!40000 ALTER TABLE `entry` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-04-22 18:11:38
