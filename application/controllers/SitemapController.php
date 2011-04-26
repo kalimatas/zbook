4d16a23ba6b476ae5acd3913616da1540de320bc
Guz Alex <kalimatas@gmail.com>
2011-04-26 17:04:29 +0400

    Added preDispatch sitemap

--- application/controllers/SitemapController.php
+++ application/controllers/SitemapController.php
@@ -3,6 +3,11 @@
  * Sitemap 
  */
 class SitemapController extends Zend_Controller_Action {
+    public function preDispatch() {
+        //$this->_helper->layout->disableLayout();
+        //$this->_helper->viewRenderer->setNoRender(true);
+    }
+
     public function indexAction() {
 
     }
